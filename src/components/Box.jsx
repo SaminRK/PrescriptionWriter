c488c1478f6a7a501333786ddc4434ca051134ae
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2020-07-09 01:37:01 +0600

    trying to add customizable boxes with editable headers

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Box.css";
+import { IconButton } from "react-bootstrap";
 
 class Box extends Component {
-    render() {
-        return (
-            <div className="box"> 
-                <input type="text" className="boxHeading" value={this.props.heading}></input>
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+
+		//this.showCloseButton = false;
+		this.onChange = this.onChange.bind(this);
+		// this.onMouseEnter = this.onMouseEnter.bind(this);
+		// this.onMouseLeave = this.onMouseLeave.bind(this);
+	}
+
+	onChange(event) {
+		this.props.handleChange(this.props.box, event.target.value);
+	}
+
+	// onMouseEnter(event) {
+	//     console.log("in");
+	//     this.setState({ showCloseButton : true });
+	// }
+
+	// onMouseLeave(event) {
+	//     console.log("out");
+	//     this.setState({ showCloseButton : false });
+	// }
+
+	render() {
+		return (
+			<div className="box">
+				<div
+					className="boxHeading"
+					//onMouseEnter={this.onMouseEnter}
+					//onMouseLeave={this.onMouseLeave}
+				>
+					<input
+						type="text"
+						className="textArea"
+						value={this.props.box.heading}
+						onChange={this.onChange}
+					></input>
+                    
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Box;
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,17 +4,30 @@ import Box from './Box';
 class Column extends Component {
     constructor(props) {
         super(props);
+        this.serialOfChildBoxes = 0;
         this.state = {
-            box_headings : ['1st box', '2nd box'],
+            boxes: [
+                { id: ++this.serialOfChildBoxes, heading: '1st box'},
+                { id: ++this.serialOfChildBoxes, heading: '2nd box'}
+            ]
         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (box, string) => {
+        const boxes = [...this.state.boxes];
+        const index = boxes.indexOf(box);
+        boxes[index].heading = string;
+        this.setState(boxes);
     }
 
     render() {
         return (
             <div> 
                 { 
-                    this.state.box_headings.map((heading) => (
-                        <Box key={heading} heading={heading}/>
+                    this.state.boxes.map((box, index) => (
+                        <Box key={box.id} box={box} handleChange={this.handleChange}/>
                     ))
                 }
             </div>
