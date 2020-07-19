7cef2262149cbd5420c5878ea98dcf31ad711650
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2020-07-19 20:11:31 +0600

    Fixed Suggestion Box Position

--- src/components/SuggestionBox.jsx
+++ src/components/SuggestionBox.jsx
@@ -5,8 +5,19 @@ import "./SuggestionBox.css";
 class SuggestionBox extends Component {
 	render() {
 		if (this.props.suggestions.length === 0) return null;
+		const coords = window
+			.getSelection()
+			.getRangeAt(0)
+			.getBoundingClientRect();
 		return (
-			<div className="suggestionBox">
+			<div
+				className="suggestionBox"
+				style={{
+					position: "absolute",
+					top: coords.y + 25,
+					left: coords.x,
+				}}
+			>
 				{this.props.word}
 				<ul>
 					{this.props.suggestions.map((word, index) => (
