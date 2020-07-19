c265c16b515da62cf0048a3c75a203329e6f7a73
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2020-07-19 23:10:53 +0600

    small correction

---
+++ build/precache-manifest.9adbfbc2f7832c3a59f3eeb4dbabbcb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ec9ba78e9dd9fea09a2fe3a5675c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ee68e97149b1925e0cb",
+    "url": "/static/css/2.0949ee41.chunk.css"
+  },
+  {
+    "revision": "baf31227630d7dc8839c",
+    "url": "/static/css/main.e1a946c1.chunk.css"
+  },
+  {
+    "revision": "1ee68e97149b1925e0cb",
+    "url": "/static/js/2.72ad40d4.chunk.js"
+  },
+  {
+    "revision": "3269388199f2e0dc65abbcc0bf911791",
+    "url": "/static/js/2.72ad40d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf31227630d7dc8839c",
+    "url": "/static/js/main.39dce826.chunk.js"
+  },
+  {
+    "revision": "092db64967d2ec5193c1",
+    "url": "/static/js/runtime-main.87bab4e5.js"
+  }
+]);
